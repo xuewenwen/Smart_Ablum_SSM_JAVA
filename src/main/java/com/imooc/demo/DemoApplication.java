552affdc5550edd784030f624e13aa38321adb06
XueJianWen <782877160@qq.com>
2019-03-16 11:42:43 +0800

    jsp

--- src/main/java/com/imooc/demo/DemoApplication.java
+++ src/main/java/com/imooc/demo/DemoApplication.java
@@ -2,10 +2,16 @@ package com.imooc.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application){
+		return  application.sources(DemoApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
