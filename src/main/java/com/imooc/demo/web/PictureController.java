4d6acc8e6ee39a5cb246696fdb8dc5c833a4b726
root <591790664@qq.com>
2019-04-21 18:02:08 +0800

    baigege

--- src/main/java/com/imooc/demo/web/PictureController.java
+++ src/main/java/com/imooc/demo/web/PictureController.java
@@ -15,8 +15,9 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.File;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
+import java.net.URLEncoder;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -98,15 +99,34 @@ public class PictureController {
             try {
                 //保存该图片
                 file.transferTo(dest);
-                //保存缩略图的原图片路径
-                //file.transferTo(pictureThumbnail);
-                //存储的缩略图路径
-                thumbnailPicPath = ImagUtil.generateThumbnail2Directory(thumbnailFilePath,picturePath);
             } catch (IOException e) {
                 e.printStackTrace();
-                result.put("msg", "IOException e");
+                result.put("msg", "FileUploadException e");
                 return result;
             }
+            ImageIcon icon = new ImageIcon(picturePath);
+            int imageWidth = icon.getIconWidth();
+            int imageHeight = icon.getIconHeight();
+                //保存缩略图的原图片路径
+
+            double shrinkWidth = 200;
+            double shrinkHeight = 150;
+           if(shrinkWidth < imageWidth)
+               shrinkWidth /=  imageWidth;
+           else shrinkWidth=1;
+            if(shrinkHeight < imageHeight)
+                shrinkHeight /=  imageHeight;
+            else shrinkWidth=1;
+                //file.transferTo(pictureThumbnail);
+                //存储的缩略图路径
+            try{
+                thumbnailPicPath = com.imooc.demo.utils.ImagUtil.generateThumbnail2Directory(shrinkWidth,shrinkHeight,thumbnailFilePath,picturePath);
+            }catch (IOException e){
+                e.printStackTrace();
+                result.put("msg", "ConvertToThumbnailException e");
+            }
+
+
             //在数据库存储的缩略图的详细路径
             //这步是将从imageutil中获取的路径值的图片名获取下来
             String thumbnail = StringUtils.substringAfter(thumbnailPicPath,"/"+id+"/");
@@ -151,4 +171,104 @@ public class PictureController {
     public String insert(ModelMap model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         return "/insert";
     }
+
+    /*@RequestMapping(value="/downd")
+    public void download(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        //logger.info("ajax download file");
+        String fileName = req.getParameter("fileName");
+        File file = new File(System.getProperty("user.home"), fileName);
+
+        resp.setContentType("application/octet-stream");
+        resp.setHeader("Content-Disposition","attachment;filename=" + fileName);
+        resp.setContentLength((int) file.length());
+
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            byte[] buffer = new byte[128];
+            int count = 0;
+            while ((count = fis.read(buffer)) > 0) {
+                resp.getOutputStream().write(buffer, 0, count);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            resp.getOutputStream().flush();
+            resp.getOutputStream().close();
+            fis.close();
+        }
+
+    }*/
+
+
+
+
+
+    //实现Spring Boot 的文件下载功能，映射网址为/download
+    @RequestMapping("/downloaded")
+    public String downloadFile(HttpServletRequest request,
+                               HttpServletResponse response) throws UnsupportedEncodingException {
+
+        // 获取指定目录下的第一个文件
+        File scFileDir = new File("D:\\沉泣.jpg");
+        File TrxFiles[] = scFileDir.listFiles();
+        //System.out.println(TrxFiles[0]);
+        String fileName = TrxFiles[0].getName(); //下载的文件名
+
+        // 如果文件名不为空，则进行下载
+        if (fileName != null) {
+            //设置文件路径
+            String realPath = "E://mubu/";
+            File file = new File(realPath, fileName);
+
+            // 如果文件名存在，则进行下载
+            if (file.exists()) {
+
+                // 配置文件下载
+                response.setHeader("content-type", "application/octet-stream");
+                response.setContentType("application/octet-stream");
+                // 下载文件能正常显示中文
+                response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+
+                // 实现文件下载
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    System.out.println("Download the song successfully!");
+                }
+                catch (Exception e) {
+                    System.out.println("Download the song failed!");
+                }
+                finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+
 }
