4b1599e203100a65d1be956b510e2f2a9fc4e75a
nekoholic <nekoholicx@gmail.com>
2019-04-21 15:47:29 +0800

    完成了更改密码的功能编写

--- src/main/java/com/imooc/demo/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.imooc.demo.service;
-
-public interface EmailService {
-    public void sendSimpleMail(String to,String title,String content);
-    //public void sendAttachmentsMail(String to, String title, String content, List<File> fileList);
-}
